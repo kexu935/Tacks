d9304d717991aedd9ea72891cf5af26010d71db3
kexu935 <xukekekekeke@gmail.com>
2018-04-14 17:50:29 -0700

    toggle redis

--- service/main.go
+++ service/main.go
@@ -31,7 +31,7 @@ const (
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://35.231.36.134:9200"
 	BUCKET_NAME = "post-images-213161"
-	ENABLE_MEMCACHE = true
+	ENABLE_MEMCACHE = false
 	REDIS_URL       = "redis-16028.c9.us-east-1-2.ec2.cloud.redislabs.com:16028"
 )
 
