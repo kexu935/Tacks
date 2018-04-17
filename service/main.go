8bd3fb71a6e3948017a555df7b2e9f7bcce27a5c
kexu935 <xukekekekeke@gmail.com>
2018-04-17 13:34:16 -0700

    adjust

--- service/main.go
+++ service/main.go
@@ -29,7 +29,7 @@ const (
 	//PROJECT_ID = "united-axle-194922"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.231.36.134:9200"
+	ES_URL = "http://35.190.128.75:9200"
 	BUCKET_NAME = "post-images-213161"
 	ENABLE_MEMCACHE = false
 	REDIS_URL       = "redis-16028.c9.us-east-1-2.ec2.cloud.redislabs.com:16028"
